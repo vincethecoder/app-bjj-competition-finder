cb5961dd058dc603872eff7681c60c8cde046c51
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-14 18:16:57 -0700

    Give a URL instead of hardcoding it in the RemoteCompetitionListLoader

--- PeteBJJCompFeed/PeteBJJCompFeedTests/RemoteCompetitionListLoaderTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeedTests/RemoteCompetitionListLoaderTests.swift
@@ -8,14 +8,16 @@
 import XCTest
 
 class RemoteCompetitionListLoader {
+    let url: URL
     let client: HTTPClient
     
-    init(client: HTTPClient) {
+    init(url: URL, client: HTTPClient) {
+        self.url = url
         self.client = client
     }
 
     func load() {
-        client.get(from: URL(string: "https://a-url.com")!)
+        client.get(from: url)
     }
 }
 
@@ -35,17 +37,19 @@ final class RemoteCompetitionListLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
         let client = HTTPClientSpy()
-        _ = RemoteCompetitionListLoader(client: client)
+        let url = URL(string: "https://a-url.com")!
+        _ = RemoteCompetitionListLoader(url: url, client: client)
         
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
+        let url = URL(string: "https://a-given-url.com")!
         let client = HTTPClientSpy()
-        let sut = RemoteCompetitionListLoader(client: client)
+        let sut = RemoteCompetitionListLoader(url: url, client: client)
         
         sut.load()
 
-        XCTAssertNotNil(client.requestedURL)
+        XCTAssertEqual(url, client.requestedURL)
     }
 }
