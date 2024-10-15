6b9d8c87fe11fae8d7b3ee23e76ebfe6b55ffc7b
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-14 18:06:01 -0700

    Move test logic to a separate type `HTTPClientSpy`

--- PeteBJJCompFeed/PeteBJJCompFeedTests/RemoteCompetitionListLoaderTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeedTests/RemoteCompetitionListLoaderTests.swift
@@ -8,20 +8,42 @@
 import XCTest
 
 class RemoteCompetitionListLoader {
-    
+    func load() {
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
+    }
 }
 
 class HTTPClient {
+    static var shared = HTTPClient()
+
+    func get(from url: URL) {  }
+}
+
+class HTTPClientSpy: HTTPClient {
     var requestedURL: URL?
+    
+    override func get(from url: URL) {
+        requestedURL = url
+    }
 }
 
 final class RemoteCompetitionListLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient()
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         _ = RemoteCompetitionListLoader()
         
         XCTAssertNil(client.requestedURL)
     }
+    
+    func test_load_requestDataFromURL() {
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
+        let sut = RemoteCompetitionListLoader()
+        
+        sut.load()
 
+        XCTAssertNotNil(client.requestedURL)
+    }
 }
