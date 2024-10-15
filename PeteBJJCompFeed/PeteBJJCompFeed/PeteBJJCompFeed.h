e0617beb8095af52e61edb0f63ec671adf31b134
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-14 17:27:20 -0700

    Create BJJ Competition Feed Framework

---
+++ PeteBJJCompFeed/PeteBJJCompFeed/PeteBJJCompFeed.h
@@ -0,0 +1,18 @@
+//
+//  PeteBJJCompFeed.h
+//  PeteBJJCompFeed
+//
+//  Created by Kobe Sam on 10/14/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PeteBJJCompFeed.
+FOUNDATION_EXPORT double PeteBJJCompFeedVersionNumber;
+
+//! Project version string for PeteBJJCompFeed.
+FOUNDATION_EXPORT const unsigned char PeteBJJCompFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PeteBJJCompFeed/PublicHeader.h>
+
+
