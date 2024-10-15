1ad6cb968f79c088ac9d15939428ffe55f0f09f4
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-15 11:29:01 -0700

    Guarantee that we request more than once

--- PeteBJJCompFeed/PeteBJJCompFeedTests/RemoteCompetitionListLoaderTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeedTests/RemoteCompetitionListLoaderTests.swift
@@ -25,6 +25,15 @@ final class RemoteCompetitionListLoaderTests: XCTestCase {
         XCTAssertEqual(url, client.requestedURL)
     }
     
+    func test_load_requestsDataFromURLTwice() {
+        let url = URL(string: "https://a-given-url.com")!
+        let (sut, client) = makeSUT(url: url)
+        sut.load()
+        sut.load()
+
+        XCTAssertEqual(client.requestedURLs, [url, url])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteCompetitionListLoader, client: HTTPClientSpy) {
@@ -35,9 +44,11 @@ final class RemoteCompetitionListLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURL: URL?
+        var requestedURLs: [URL] = []
         
         func get(from url: URL) {
             requestedURL = url
+            requestedURLs.append(url)
         }
     }
 }
