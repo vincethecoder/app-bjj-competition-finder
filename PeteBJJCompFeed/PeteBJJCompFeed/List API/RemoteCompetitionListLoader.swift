e8ef9972023c07391718cadcb7faa9afd1944a65
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-16 00:24:20 -0700

    Handling Errors + Stubbing vs. Spying + Eliminating Invalid Paths
    Learning Outcomes
    - Handling network errors
    - Differences between stubbing and spying when unit testing
    - How to extend code coverage by using samples of values to test specific test cases
    - Design better code with enums to make invalid paths unrepresentable

--- PeteBJJCompFeed/PeteBJJCompFeed/List API/RemoteCompetitionListLoader.swift
+++ PeteBJJCompFeed/PeteBJJCompFeed/List API/RemoteCompetitionListLoader.swift
@@ -7,8 +7,13 @@
 
 import Foundation
 
+public enum HTTPClientResult {
+    case success(HTTPURLResponse)
+    case failure(Error)
+}
+
 public protocol HTTPClient {
-    func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void)
+    func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void)
 }
 
 public final class RemoteCompetitionListLoader {
@@ -26,11 +31,13 @@ public final class RemoteCompetitionListLoader {
     }
 
     public func load(completion: @escaping (Error) -> Void) {
-        client.get(from: url) { error, response in
-            if response != nil {
+        client.get(from: url) { result in
+            switch result {
+            case .success:
                 completion(.invalidData)
-            } else {
+            case .failure:
                 completion(.connectivity)
+                
             }
         }
     }
--- PeteBJJCompFeed/PeteBJJCompFeedTests/RemoteCompetitionListLoaderTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeedTests/RemoteCompetitionListLoaderTests.swift
@@ -72,14 +72,14 @@ final class RemoteCompetitionListLoaderTests: XCTestCase {
             self.messages.map { $0.url }
         }
         
-        private var messages = [(url: URL, completion: (Error?, HTTPURLResponse?) -> Void)]()
+        private var messages = [(url: URL, completion: (HTTPClientResult) -> Void)]()
         
-        func get(from url: URL, completion: @escaping (Error?, HTTPURLResponse?) -> Void) {
+        func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
             messages.append((url, completion))
         }
         
         func complete(with error: Error, at index: Int = 0) {
-            messages[index].completion(error, nil)
+            messages[index].completion(.failure(error))
         }
         
         func complete(withStatusCode code: Int, at index: Int = 0) {
@@ -88,8 +88,8 @@ final class RemoteCompetitionListLoaderTests: XCTestCase {
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil
-            )
-            messages[index].completion(nil, response)
+            )!
+            messages[index].completion(.success(response))
         }
     }
 }
