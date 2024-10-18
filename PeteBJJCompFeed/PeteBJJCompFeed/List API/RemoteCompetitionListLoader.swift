1fb01301fc5b6dbcdff627b1009729b9ad0f8ea1
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-18 11:09:10 -0700

    Replace magic number with variable giving extra context

--- PeteBJJCompFeed/PeteBJJCompFeed/List API/RemoteCompetitionListLoader.swift
+++ PeteBJJCompFeed/PeteBJJCompFeed/List API/RemoteCompetitionListLoader.swift
@@ -56,9 +56,11 @@ private enum CompetitionListMapper {
     private struct Root: Decodable {
         let competitions: [Competition]
     }
+    
+    static var OK_200: Int { 200 }
 
     static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [Competition] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteCompetitionListLoader.Error.invalidData
         }
         return try JSONDecoder().decode(Root.self, from: data).competitions
