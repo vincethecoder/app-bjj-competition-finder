ca9590ddc44a97c5824c14060ab17ebbbece1d90
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-18 11:29:35 -0700

    Move `CompetitionListMapper` to its own file

--- PeteBJJCompFeed/PeteBJJCompFeed/List API/RemoteCompetitionListLoader.swift
+++ PeteBJJCompFeed/PeteBJJCompFeed/List API/RemoteCompetitionListLoader.swift
@@ -42,19 +42,3 @@ public final class RemoteCompetitionListLoader {
         }
     }
 }
-
-private enum CompetitionListMapper {
-    private struct Root: Decodable {
-        let competitions: [Competition]
-    }
-    
-    static var OK_200: Int { 200 }
-
-    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [Competition] {
-        guard response.statusCode == OK_200 else {
-            throw RemoteCompetitionListLoader.Error.invalidData
-        }
-        return try JSONDecoder().decode(Root.self, from: data).competitions
-    }
-}
-
