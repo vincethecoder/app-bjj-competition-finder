c7c80ea3b1a875b09f73454d884f1b70363460f2
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-20 03:53:42 -0700

    Move URL creation to a shared factory computed property

--- PeteBJJCompFeed/PeteBJJCompFeedTests/List API/URLSessionHTTPClientTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeedTests/List API/URLSessionHTTPClientTests.swift
@@ -39,7 +39,7 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_performsGETRequestWithURL() {
-        let url = URL(string: "http://any-url.com")!
+        let url = anyURL
         let exp = expectation(description: "Wait for request")
         
         URLProtocolStub.observeRequests { request in
@@ -54,13 +54,12 @@ final class URLSessionHTTPClientTests: XCTestCase {
     }
     
     func test_getFromURL_failsOnRequestError(){
-        let url = URL(string: "http://any-url.com")!
         let error = NSError(domain: "any error", code: 1)
         URLProtocolStub.stub(data: nil, response: nil, error: error)
         
         let exp = expectation(description: "Wait for completion")
 
-        makeSUT().get(from: url) { result in
+        makeSUT().get(from: anyURL) { result in
             switch result {
             case let .failure(receivedError as NSError):
                 XCTAssertEqual(receivedError.domain, error.domain)
@@ -82,6 +81,10 @@ final class URLSessionHTTPClientTests: XCTestCase {
         return sut
     }
 
+    private var anyURL: URL {
+        URL(string: "http://any-url.com")!
+    }
+    
     private final class URLProtocolStub: URLProtocol {
         private static var stub: Stub?
         private static var requestObserver: ((URLRequest) -> Void)?
