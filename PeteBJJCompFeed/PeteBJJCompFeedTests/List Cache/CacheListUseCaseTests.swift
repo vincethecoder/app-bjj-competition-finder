c2abbf03c47f096d665c995f0d527c37a4388235
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-21 13:48:49 -0700

    Save command requests cache deletion

--- PeteBJJCompFeed/PeteBJJCompFeedTests/List Cache/CacheListUseCaseTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeedTests/List Cache/CacheListUseCaseTests.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import PeteBJJCompFeed
 
 class LocalListLoader {
+    private let store: ListStore
+    
     init(store: ListStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [Competition]) {
+        store.deleteCachedList()
     }
 }
 
 class ListStore {
     var deleteCachedListCallCount = 0
+    
+    func deleteCachedList() {
+        deleteCachedListCallCount += 1
+    }
 }
 
 class CacheListUseCaseTests: XCTestCase {
@@ -25,4 +36,21 @@ class CacheListUseCaseTests: XCTestCase {
         
         XCTAssertEqual(store.deleteCachedListCallCount, 0)
     }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = ListStore()
+        let sut = LocalListLoader(store: store)
+        let items = [uniqueItem, uniqueItem]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedListCallCount, 1)
+    }
+    
+    // MARK: - Helper
+    
+    private var uniqueItem: Competition {
+        Competition(id: UUID().uuidString, name: "any-name", startDate: Date(), endDate: Date(), venue: "any-venue", city: "any-city", state: nil, country: "any-country", type: .gi, status: .upcoming, registrationStatus: .notOpen, registrationLink: nil, eventLink: URL(string: "https://any-event-link.com")!, categories: [.adult], rankingPoints: 0, notes: nil)
+    }
+    
 }
