09b2cd6d755fd51e80bcc87241f65b662d58a7f8
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-21 21:04:36 -0700

    Invert if logic to make code paths easier to follow

--- PeteBJJCompFeed/PeteBJJCompFeedTests/List Cache/CacheListUseCaseTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeedTests/List Cache/CacheListUseCaseTests.swift
@@ -20,13 +20,14 @@ class LocalListLoader {
     func save(_ items: [Competition], completion: @escaping (Error?) -> Void) {
         store.deleteCachedList { [weak self] error in
             guard let self else { return }
-            if error == nil {
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
                 self.store.insert(items, timestamp: self.currentDate()){ [weak self] error in
                     guard self != nil else { return }
                     completion(error)
                 }
-            } else {
-                completion(error)
+                
             }
         }
     }
