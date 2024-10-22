16eeb877a2293a9afdd0c84b6f5f0e5a5d9e045f
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-21 21:02:59 -0700

    Guarantee that the LocalFeedLoader does not deliver insertion error after instance has been deallocated

--- PeteBJJCompFeed/PeteBJJCompFeedTests/List Cache/CacheListUseCaseTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeedTests/List Cache/CacheListUseCaseTests.swift
@@ -21,7 +21,10 @@ class LocalListLoader {
         store.deleteCachedList { [weak self] error in
             guard let self else { return }
             if error == nil {
-                self.store.insert(items, timestamp: self.currentDate(), completion: completion)
+                self.store.insert(items, timestamp: self.currentDate()){ [weak self] error in
+                    guard self != nil else { return }
+                    completion(error)
+                }
             } else {
                 completion(error)
             }
@@ -113,6 +116,20 @@ class CacheListUseCaseTests: XCTestCase {
         
         sut = nil
         store.completeDeletion(with: anyNSError)
+        
+        XCTAssertTrue(receivedResults.isEmpty)
+    }
+    
+    func test_save_doesNotDeliverInsertionErrorAfterSUTInstanceHasBeenDeallocated() {
+        let store = ListStoreSpy()
+        var sut: LocalListLoader? = LocalListLoader(store: store, currentDate: Date.init)
+        
+        var receivedResults = [Error?]()
+        sut?.save([uniqueItem]) { receivedResults.append($0) }
+        
+        store.completeDeletionSuccessfully()
+        sut = nil
+        store.completeInsertion(with: anyNSError)
         
         XCTAssertTrue(receivedResults.isEmpty)
     }
