41c99e4299e0a17e680d35fcfa073796f6450b4d
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-26 20:42:47 -0700

    Add empty CoreData competitions store specs

---
+++ PeteBJJCompFeed/PeteBJJCompFeedTests/Competitions Cache/CoreDataCompetitionsStoreTests.swift
@@ -0,0 +1,60 @@
+//
+//  CoreDataCompetitionsStoreTests.swift
+//  PeteBJJCompFeedTests
+//
+//  Created by Kobe Sam on 10/26/24.
+//
+
+import XCTest
+import PeteBJJCompFeed
+
+class CoreDataCompetitionsStoreTests: XCTestCase, CompetitionsStoreSpecs {
+    
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+}
