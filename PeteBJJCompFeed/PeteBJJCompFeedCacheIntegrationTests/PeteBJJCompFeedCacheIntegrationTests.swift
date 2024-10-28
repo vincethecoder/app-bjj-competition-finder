38e7aa820cd8c78598583be265391dc5d04003ff
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-28 15:36:39 -0700

    LocalCompetitionLoader in integration with the CoreDataCompetitionStore delivers items saved on separate instances, proving we correctly persist the data models to disk.

--- PeteBJJCompFeed/PeteBJJCompFeedCacheIntegrationTests/PeteBJJCompFeedCacheIntegrationTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeedCacheIntegrationTests/PeteBJJCompFeedCacheIntegrationTests.swift
@@ -32,10 +32,12 @@ final class PeteBJJCompFeedCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        // MARK: TODO
+    }
+    
     // MARK: Helpers
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalCompetitionsLoader {
-        let storeBundle = Bundle(for: CoreDataCompetitionsStore.self)
-        let storeURL = testSpecificStoreURL()
         let store = CoreDataCompetitionsStore()
         let sut = LocalCompetitionsLoader(store: store, currentDate: Date.init)
         trackForMemoryLeaks(store, file: file, line: line)
@@ -49,5 +51,17 @@ final class PeteBJJCompFeedCacheIntegrationTests: XCTestCase {
     
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private var uniqueCompetition: Competition {
+        Competition(id: UUID().uuidString, name: "any-name", startDate: Date(), endDate: Date(), venue: "any-venue", city: "any-city", state: nil, country: "any-country", type: .gi, status: .upcoming, registrationStatus: .notOpen, registrationLink: nil, eventLink: anyURL, categories: [.adult], rankingPoints: 0, notes: nil)
+    }
+    
+    private var uniqueCompetitions: (models: [Competition], local: [LocalCompetition]) {
+        let models = [uniqueCompetition, uniqueCompetition]
+        let localCompetitions = models.map {
+            LocalCompetition(id: $0.id, name: $0.name, startDate: $0.startDate, endDate: $0.endDate, venue: $0.venue, city: $0.city, state: $0.state, country: $0.country, type: $0.type, status: $0.status, registrationStatus: $0.registrationStatus, registrationLink: $0.registrationLink, eventLink: $0.eventLink, categories: $0.categories, rankingPoints: $0.rankingPoints, notes: $0.notes)
+        }
+        return (models, localCompetitions)
     }
 }
