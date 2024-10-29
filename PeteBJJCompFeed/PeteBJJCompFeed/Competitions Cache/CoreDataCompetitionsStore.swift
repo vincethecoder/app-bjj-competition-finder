385f3fef37180873e9ca97b6b653fcf11710a8e8
Vince (Kobe) Sam <kobesam@gmail.com>
2024-10-29 08:00:12 -0700

    Add ManagedCompetitions extensions

--- PeteBJJCompFeed/PeteBJJCompFeed/Competitions Cache/CoreDataCompetitionsStore.swift
+++ PeteBJJCompFeed/PeteBJJCompFeed/Competitions Cache/CoreDataCompetitionsStore.swift
@@ -23,3 +23,28 @@ public final class CoreDataCompetitionsStore: CompetitionsStore {
 
     }
 }
+
+extension ManagedCompetitions {
+    var type: CompetitionType {
+        get { CompetitionType(rawValue: typeRawValue!) ?? .gi }
+        set { typeRawValue = newValue.rawValue }
+    }
+    
+    var status: CompetitionStatus {
+        get { CompetitionStatus(rawValue: statusRawValue!) ?? .upcoming }
+        set { statusRawValue = newValue.rawValue }
+    }
+    
+    var registrationStatus: RegistrationStatus {
+        get { RegistrationStatus(rawValue: registrationStatusRawValue!) ?? .closed }
+        set { registrationStatusRawValue = newValue.rawValue }
+    }
+    
+    var categories: Set<CompetitionCategory> {
+        get {
+            guard let categoryValues = categoriesRawValue?.components(separatedBy: ",") else { return [] }
+            return Set(categoryValues.compactMap { CompetitionCategory(rawValue: $0) })
+        }
+        set { categoriesRawValue = newValue.map { $0.rawValue }.joined(separator: ",") }
+    }
+}
