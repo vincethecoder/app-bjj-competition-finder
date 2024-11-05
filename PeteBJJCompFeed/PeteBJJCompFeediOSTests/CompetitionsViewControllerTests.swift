62ceb146efc6ff833a4d3b0c8277b6147c608f2d
Vince (Kobe) Sam <kobesam@gmail.com>
2024-11-04 16:50:42 -0800

    Feed image view loading indicator is visible while loading image

--- PeteBJJCompFeed/PeteBJJCompFeediOSTests/CompetitionsViewControllerTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeediOSTests/CompetitionsViewControllerTests.swift
@@ -95,6 +95,46 @@ final class CompetitionsViewControllerTests: XCTestCase {
         XCTAssertEqual(loader.loadedImageURLs, [image01.url, image02.url], "Expected second image URL request once second view also becomes visible")
     }
     
+    func test_feedImageView_cancelsImageLoadingWhenNotVisibleAnymore() {
+        let competitions = uniqueCompetitions.models
+        let (event01, event02) = (competitions[0], competitions[1])
+        let (sut, loader) = makeSUT()
+        
+        sut.simulateAppearance()
+        loader.completeFeedLoading(with: [event01, event02])
+        XCTAssertEqual(loader.cancelledImageURLs, [], "Expected no cancelled image URL requests until image is not visible")
+        
+        // MARK: - TODO
+        
+//        sut.simulateCompetitionViewNotVisible(at: 0)
+//        XCTAssertEqual(loader.cancelledImageURLs, [image0.url], "Expected one cancelled image URL request once first image is not visible anymore")
+//        
+//        sut.simulateFeedImageViewNotVisible(at: 1)
+//        XCTAssertEqual(loader.cancelledImageURLs, [image0.url, image1.url], "Expected two cancelled image URL requests once second image is also not visible anymore")
+    }
+    
+    func test_feedImageViewLoadingIndicator_isVisibleWhileLoadingImage() {
+        let competitions = uniqueCompetitions.models
+        let (event01, event02) = (competitions[0], competitions[1])
+        let (sut, loader) = makeSUT()
+        
+        sut.simulateAppearance()
+        loader.completeFeedLoading(with: [event01, event02])
+
+        let view0 = sut.simulateCompetitionViewVisible(at: 0)
+        let view1 = sut.simulateCompetitionViewVisible(at: 1)
+        XCTAssertEqual(view0?.isShowingImageLoadingIndicator, true, "Expected loading indicator for first view while loading first image")
+        XCTAssertEqual(view1?.isShowingImageLoadingIndicator, true, "Expected loading indicator for second view while loading second image")
+        
+        loader.completeImageLoading(at: 0)
+        XCTAssertEqual(view0?.isShowingImageLoadingIndicator, false, "Expected no loading indicator for first view once first image loading completes successfully")
+        XCTAssertEqual(view1?.isShowingImageLoadingIndicator, true, "Expected no loading indicator state change for second view once first image loading completes successfully")
+        
+        loader.completeImageLoadingWithError(at: 1)
+        XCTAssertEqual(view0?.isShowingImageLoadingIndicator, false, "Expected no loading indicator state change for first view once second image loading completes with error")
+        XCTAssertEqual(view1?.isShowingImageLoadingIndicator, false, "Expected no loading indicator for second view once second image loading completes with error")
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: CompetitionsViewController, loader: LoaderSpy) {
@@ -182,11 +222,11 @@ final class CompetitionsViewControllerTests: XCTestCase {
             imageRequests.map { $0.url }
         }
         
-        private(set) var cacelledImageURLs = [URL]()
+        private(set) var cancelledImageURLs = [URL]()
         
         func loadImageData(from url: URL, completition: @escaping (EventImageDataLoader.Result) -> Void) -> any EventImageDataLoaderTask {
             imageRequests.append((url, completition))
-            return TaskSpy { [weak self] in self?.cacelledImageURLs.append(url) }
+            return TaskSpy { [weak self] in self?.cancelledImageURLs.append(url) }
         }
         
         func completeImageLoading(with imageData: Data = Data(), at index: Int = 0) {
