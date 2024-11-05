e32b5b9a919bf7fb726410a0f06d98f2c7da309b
Vince (Kobe) Sam <kobesam@gmail.com>
2024-11-04 21:01:17 -0800

    iOS 16.1+ fix: Set explicit test images scale to 1 to ensure they all have the same scale (so their pngData can be compared pixel by pixel)

--- PeteBJJCompFeed/PeteBJJCompFeediOSTests/CompetitionsViewControllerTests.swift
+++ PeteBJJCompFeed/PeteBJJCompFeediOSTests/CompetitionsViewControllerTests.swift
@@ -136,27 +136,27 @@ final class CompetitionsViewControllerTests: XCTestCase {
     }
     
     func test_feedImageView_rendersImageLoadedFromURL() {
-//        let competitions = uniqueCompetitions.models
-//        let (event01, event02) = (competitions[0], competitions[1])
-//        let (sut, loader) = makeSUT()
-//
-//        sut.simulateAppearance()
-//        loader.completeFeedLoading(with: [event01, event02])
-//
-//        let view0 = sut.simulateCompetitionViewVisible(at: 0)
-//        let view1 = sut.simulateCompetitionViewVisible(at: 1)
-//        XCTAssertEqual(view0?.renderedImage, .none, "Expected no image for first view while loading first image")
-//        XCTAssertEqual(view1?.renderedImage, .none, "Expected no image for second view while loading second image")
-//
-//        let imageData0 = UIImage.make(withColor: .red).pngData()!
-//        loader.completeImageLoading(with: imageData0, at: 0)
-//        XCTAssertEqual(view0?.renderedImage, imageData0, "Expected image for first view once first image loading completes successfully")
-//        XCTAssertEqual(view1?.renderedImage, .none, "Expected no image state change for second view once first image loading completes successfully")
-//
-//        let imageData1 = UIImage.make(withColor: .blue).pngData()!
-//        loader.completeImageLoading(with: imageData1, at: 1)
-//        XCTAssertEqual(view0?.renderedImage, imageData0, "Expected no image state change for first view once second image loading completes successfully")
-//        XCTAssertEqual(view1?.renderedImage, imageData1, "Expected image for second view once second image loading completes successfully")
+        let competitions = uniqueCompetitions.models
+        let (event01, event02) = (competitions[0], competitions[1])
+        let (sut, loader) = makeSUT()
+
+        sut.simulateAppearance()
+        loader.completeFeedLoading(with: [event01, event02])
+
+        let view0 = sut.simulateCompetitionViewVisible(at: 0)
+        let view1 = sut.simulateCompetitionViewVisible(at: 1)
+        XCTAssertEqual(view0?.renderedImage, .none, "Expected no image for first view while loading first image")
+        XCTAssertEqual(view1?.renderedImage, .none, "Expected no image for second view while loading second image")
+
+        let imageData0 = UIImage.make(withColor: .red).pngData()!
+        loader.completeImageLoading(with: imageData0, at: 0)
+        XCTAssertEqual(view0?.renderedImage, imageData0, "Expected image for first view once first image loading completes successfully")
+        XCTAssertEqual(view1?.renderedImage, .none, "Expected no image state change for second view once first image loading completes successfully")
+
+        let imageData1 = UIImage.make(withColor: .blue).pngData()!
+        loader.completeImageLoading(with: imageData1, at: 1)
+        XCTAssertEqual(view0?.renderedImage, imageData0, "Expected no image state change for first view once second image loading completes successfully")
+        XCTAssertEqual(view1?.renderedImage, imageData1, "Expected image for second view once second image loading completes successfully")
     }
     
     
@@ -424,13 +424,13 @@ private extension UIButton {
 private extension UIImage {
     static func make(withColor color: UIColor) -> UIImage {
         let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
-        UIGraphicsBeginImageContextWithOptions(rect.size, false, 1.0)
-        let context = UIGraphicsGetCurrentContext()!
-        context.setFillColor(color.cgColor)
-        context.fill(rect)
-        let img = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return img
+        let format = UIGraphicsImageRendererFormat()
+        format.scale = 1
+        
+        return UIGraphicsImageRenderer(size: rect.size, format: format).image { rendererContext in
+            color.setFill()
+            rendererContext.fill(rect)
+        }
     }
 }
 
