5f2413b551c55eee7733d8c19b6f281635da3a95
Vince (Kobe) Sam <kobesam@gmail.com>
2024-11-05 16:44:16 -0800

    Extract FeedImageCell creation/configuration and image loading logic with FeedImageDataLoader to the new FeedImageCellController

--- PeteBJJCompFeed/PeteBJJCompFeediOS/Feed UI/Controllers/CompetitionsViewController.swift
+++ PeteBJJCompFeed/PeteBJJCompFeediOS/Feed UI/Controllers/CompetitionsViewController.swift
@@ -15,7 +15,7 @@ public final class CompetitionsViewController: UITableViewController, UITableVie
     private var tableModel = [Competition]() {
         didSet { tableView.reloadData() }
     }
-    private var tasks = [IndexPath: EventImageDataLoaderTask]()
+    private var cellControllers = [IndexPath: CompetitionsCellController]()
     
     public convenience init(competitionLoader: CompetitionsLoader, imageLoader: EventImageDataLoader) {
         self.init()
@@ -52,52 +52,32 @@ public final class CompetitionsViewController: UITableViewController, UITableVie
     }
     
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellModel = tableModel[indexPath.row]
-        let cell = CompetitionsCell()
-        let event = cellModel.toCompetitiveEvent()
-        cell.dateLabel.text = event.date
-        cell.eventLabel.text = event.name
-        cell.venueLabel.text = event.venue
-        cell.eventImageView.image = nil
-        cell.eventImageRetryButton.isHidden = true
-        cell.eventImageContainer.startShimmering()
-        
-        let loadImage = { [weak self, weak cell] in
-            guard let self else { return }
-            
-            self.tasks[indexPath] = self.imageLoader?.loadImageData(from: event.url) { [weak cell] result in
-                let data = try? result.get()
-                let image = data.map(UIImage.init) ?? nil
-                cell?.eventImageView.image = image
-                cell?.eventImageRetryButton.isHidden = (image != nil)
-                cell?.eventImageContainer.stopShimmering()
-            }
-        }
-        
-        cell.onRetry = loadImage
-        loadImage()
-        
-        return cell
+        let cellController = cellController(forRowAt: indexPath)
+        return cellController.view()
     }
     
     public override func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
-        cancelTask(forRowAt: indexPath)
+        removeCellController(forRowAt: indexPath)
     }
     
     public func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         indexPaths.forEach { indexPath in
-            let cellModel = tableModel[indexPath.row]
-            let event = cellModel.toCompetitiveEvent()
-            tasks[indexPath] = imageLoader?.loadImageData(from: event.url) { _ in }
+            cellController(forRowAt: indexPath).preload()
         }
     }
     
     public func tableView(_ tableView: UITableView, cancelPrefetchingForRowsAt indexPaths: [IndexPath]) {
-        indexPaths.forEach(cancelTask)
+        indexPaths.forEach(removeCellController)
+    }
+
+    private func cellController(forRowAt indexPath: IndexPath) -> CompetitionsCellController {
+        let cellModel = tableModel[indexPath.row]
+        let cellController = CompetitionsCellController(model: cellModel, imageLoader: imageLoader!)
+        cellControllers[indexPath] = cellController
+        return cellController
     }
     
-    private func cancelTask(forRowAt indexPath: IndexPath) {
-        tasks[indexPath]?.cancel()
-        tasks[indexPath] = nil
+    private func removeCellController(forRowAt indexPath: IndexPath) {
+        cellControllers[indexPath] = nil
     }
 }
